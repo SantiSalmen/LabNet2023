670e99a798da20c565959c4bac83c78b72b585b6
SantiSalmen <santisalmen@gmail.com>
2023-08-15 23:42:01 -0300

    Corrección ReadKey
     Anteriormente ejecutaba el codigo con Ctrl+F5, ya que solo con F5 (Debuggin) se cerraba la aplicacción automaticamente.
     Gracias al grupo me di cuenta que estaba teniendo un error muy basico, solo me faltaba el ReadKey para que la app me espere.

--- LabNet2023.HelloWorld/LabNet2023.HelloWorld/Program.cs
+++ LabNet2023.HelloWorld/LabNet2023.HelloWorld/Program.cs
@@ -7,6 +7,7 @@ namespace LabNet2023.HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
