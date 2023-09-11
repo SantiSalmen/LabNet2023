482e42ac02f4b2eaabaa929916bc4fd9d3c08c48
SantiSalmen <santisalmen@gmail.com>
2023-09-11 05:29:21 -0300

    Actualizacion Implementacion de una sola view para insert y update. Uso de Jquery, implementando el metodo ajax.

--- Practica3_EF/Practica3.EF.MVC/Controllers/EmployeesController.cs
+++ Practica3_EF/Practica3.EF.MVC/Controllers/EmployeesController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Practica3.EF.Entities;
+﻿using Practica3.EF.Entities;
 using Practica3.EF.Logic;
 using Practica3.EF.MVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Practica3.EF.MVC.Controllers
 {
@@ -26,33 +24,53 @@ namespace Practica3.EF.MVC.Controllers
             return View(employeesViews);
         }
 
-        public ActionResult Insert() 
+
+        public ActionResult Delete(int id)
         {
-            return View();
+            Employees employee = new Employees();
+
+            foreach (Employees eList in logic.GetAll())
+            {
+                if (id == eList.EmployeeID)
+                {
+                    employee = eList;
+
+                }
+
+            }
+            logic.Delete(employee);
+            return RedirectToAction("index");
         }
 
-        [HttpPost]
-        public ActionResult Insert(EmployeesView employeesViews) 
+        public ActionResult UpdateInsert() 
         {
-            try
+            return View(); 
+        }
+      
+        [HttpPost]
+        public ActionResult UpdateInsert(EmployeesView employee)
+        {
+
+            if (!ModelState.IsValid)
             {
-                //TODO: No permitir que ingrese un valor nulo. Id autoincremental.
-                Employees employeesEntity = new Employees { FirstName = employeesViews.firstName, LastName = employeesViews.lastName };
+                return View(employee);
+            }
+            
+            if(employee.id == 0)
+            {
+                Employees employeesEntity = new Employees { FirstName = employee.firstName, LastName = employee.lastName };
                 logic.Add(employeesEntity);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Employees employeesEntity = new Employees { EmployeeID = employee.id, FirstName = employee.firstName, LastName = employee.lastName };
+                logic.Update(employeesEntity);
                 return RedirectToAction("Index");
 
             }
-            catch (Exception)
-            {
 
-                return RedirectToAction ("Index","Error");
-            }
         }
-        //TODO: Arreglar compatibilidad con customer y el id.
-        public ActionResult Delete(Employees id) 
-        {
-            logic.Delete(id);
-            return RedirectToAction("index");
-        }
+
     }
 }
