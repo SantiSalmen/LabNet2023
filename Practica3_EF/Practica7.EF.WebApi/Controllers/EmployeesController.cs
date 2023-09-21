de637a96532705ee9f5439946c2a295dab503a60
SantiSalmen <santisalmen@gmail.com>
2023-09-21 03:32:03 -0300

    Correcciones
     me percaté que uno de los ultimos cambios rompió mi codigo y lo arreglé, admeas de algunosdetalles adicionales.

--- Practica3_EF/Practica7.EF.WebApi/Controllers/EmployeesController.cs
+++ Practica3_EF/Practica7.EF.WebApi/Controllers/EmployeesController.cs
@@ -10,7 +10,7 @@ using System.Web.Http.Cors;
 
 namespace Practica7.EF.WebApi.Controllers
 {
-    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class EmployeesController : ApiController
     {
         EmployeesLogic logic = new EmployeesLogic();
